90a91ef60ce7db748d0d5d18ef22b09d7fefc68e
AAKASHMAJUMDER70 <aakashmajumder70@gmail.com>
2024-05-04 07:09:22 +0530

    folder str and req.txt satisfied

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Aakash",
+    author_email="aakashmajumder70@gmail.com",
+    packages=find_packages()
+)
